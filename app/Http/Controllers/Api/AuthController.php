67ae61e0635977bd2c05e9ef934fd923fcb9bfc6
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2023-08-31 19:44:50 +0600

    Add auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'error' => 'The provided credentials are incorrect'
+            ], 422);
+        }
+
+        return response()->json([
+            'access_token' => $user->createToken('develop_news')->plainTextToken,
+        ], 201);
+    }
+}
